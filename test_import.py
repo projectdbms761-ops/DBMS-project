b67ca02dacf3f6c6d0cc23cb2cf08fbf3795f952
HARSH PALKRUTWAR <16.prahar@gmail.com>
2026-03-17 23:16:52 +0530

    update

--- test_import.py
+++
@@ -1,8 +0,0 @@
-import sys, traceback, importlib
-sys.path.insert(0, r"d:\DBMS project")
-try:
-    importlib.invalidate_caches()
-    m = importlib.import_module('app')
-    print('IMPORT_OK')
-except Exception:
-    traceback.print_exc()
